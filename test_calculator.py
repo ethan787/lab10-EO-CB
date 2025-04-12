bad29051c400769f12b9b8c5b7d1a7134bbec0e1
ethan787 <ethanoritz2006@gmail.com>
2025-04-12 18:18:56 -0400

    modified test calculator but better

--- test_calculator.py
+++ test_calculator.py
@@ -2,13 +2,18 @@ import unittest
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
-    ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    ######## Partner 2
+    def test_add(self): # 3 assertions
+        self.assertEqual(add(2,3),5)
+        self.assertEqual(add(-5,-4),-9)
+        self.assertNotEqual(add(3,111),9)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_subtract(self): # 3 assertions
+        self.assertEqual(sub(9,5),4)
+        self.assertEqual(sub(-9,5),-14)
+        self.assertNotEqual(sub(9,5),3)
+
+    ##########################
 
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
@@ -18,20 +23,25 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     # ##########################
 
-    ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    ####### Partner 2
+    def test_divide_by_zero(self): # 1 assertion
+        with self.assertRaises(ZeroDivisionError):
+            div(0,5)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    # call division function inside, example:
+    # with self.assertRaises(<INSERT_ERROR_TYPE>):
+    #     div(0, 5)
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
+
+    def test_logarithm(self): # 3 assertions
+        self.assertEqual(log(3,2),math.log(3,2))
+        self.assertEqual(log(2,10),math.log(2,10))
+        self.assertEqual(log(0,3),math.log(0,3))
+    def test_log_invalid_base(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            log(2,-2)
+# use same technique from test_divide_by_zero
+    ##########################
     
     ######## Partner 1
     # def test_log_invalid_argument(self): # 1 assertion
